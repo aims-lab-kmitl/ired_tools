305b01ebd00d3fe037f343a3722a3ba1bf71bd3b
Thitipong Thepsit <89779525+ThitipongTh@users.noreply.github.com>
2022-06-16 00:52:54 +0700

    Upload Install ROS

---
+++ README.md
@@ -0,0 +1,25 @@
+# KBOT Tools
+## Install ROS Noetic on PC
+Setup your PC and install Ubuntu 20.04.x on your PC
+- Ubuntu 20.04.4 LTS (Focal Fossa): https://releases.ubuntu.com/20.04/ubuntu-20.04.4-desktop-amd64.iso
+- Rufus: https://rufus.ie/
+
+```sh
+$ sudo apt update
+$ sudo apt upgrade -y
+$ wget https://raw.githubusercontent.com/aims-lab-kmitl/kbot_tools/main/aims_install_ros.sh
+$ chmod 755 ./aims_install_ros.sh
+$ bash ./aims_install_ros.sh
+```
+
+## Install ROS Noetic on Raspberry Pi
+Setup SD card and install Ubuntu 20.04.x on Raspberry Pi
+- Raspberry Pi Imager: https://www.raspberrypi.com/software/
+
+```sh
+$ sudo apt update
+$ sudo apt upgrade -y
+$ wget https://raw.githubusercontent.com/aims-lab-kmitl/kbot_tools/main/aims_install_ros_rp.sh
+$ chmod 755 ./aims_install_ros_rp.sh
+$ bash ./aims_install_ros_rp.sh
+```
---
+++ aims_install_ros_rp.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo ""
+echo "AIMs Lab x RAI KMITL"
+echo "OS version: Ubuntu 20.04.x (Focal Fossa)"
+echo "ROS version: ROS Noetic Ninjemys"
+echo ""
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+read
+
+echo "[Update the package lists]"
+sudo apt update
+sudo apt upgrade -y
+
+echo "[Setup your sources.list]"
+sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main\" > /etc/apt/sources.list.d/ros-latest.list"
+
+echo "[Set up your keys]"
+sudo apt install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+echo "[Update the package lists]"
+sudo apt update
+sudo apt upgrade -y
+
+echo "ROS-Base: (Bare Bones)"
+sudo apt install -y ros-noetic-ros-base
+
+echo "[Environment setup]"
+sh -c "echo \"source /opt/ros/noetic/setup.bash\" >> ~/.bashrc"
+source $HOME/.bashrc
+
+echo "[Dependencies for building packages]"
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+echo "[Initialize rosdep and Update]"
+sudo sh -c "rosdep init"
+rosdep update
+
+source $HOME/.bashrc
+
+echo "[Complete!!!]"
+exit 0
