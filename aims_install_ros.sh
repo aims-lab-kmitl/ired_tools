3b0dd9dbaced02b8b77b1d8d112225610e622102
Thitipong Thepsit <89779525+ThitipongTh@users.noreply.github.com>
2025-05-22 10:51:30 +0700

    Update aims_install_ros.sh

--- aims_install_ros.sh
+++ aims_install_ros.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-echo ""
-echo "AIMs Lab x RAI KMITL"
-echo "OS version: Ubuntu 20.04.x (Focal Fossa)"
-echo "ROS version: ROS Noetic Ninjemys"
-echo ""
-echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
-echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
-read
+# Exit on any error
+set -e
 
-echo "[Update the package lists]"
+# Countdown function
+countdown() {
+    echo "🚀 ROS 1 Noetic installation will begin shortly..."
+    echo "👉 Press Enter to start immediately or Ctrl+C to cancel."
+    
+    for i in {10..1}; do
+        echo -ne "⏳ Starting in $i seconds... \r"
+        read -t 1 -n 1 key && break
+    done
+    echo -e "\n🔧 Starting installation..."
+}
+
+countdown
+
+echo "[📦 Update the package lists]"
 sudo apt update
 sudo apt upgrade -y
 
-echo "[Setup your sources.list]"
+echo "[🔑 Setup your sources.list]"
 sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main\" > /etc/apt/sources.list.d/ros-latest.list"
 
-echo "[Set up your keys]"
+echo "🌐 [Set up your keys]"
 sudo apt install -y curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
@@ -24,11 +32,12 @@ echo "[Update the package lists]"
 sudo apt update
 sudo apt upgrade -y
 
-echo "ROS-Base: (Bare Bones)"
+echo "📥 ROS-Desktop: (Desktop Full)"
 sudo apt install -y ros-noetic-desktop-full
 
 echo "[Environment setup]"
-sh -c "echo \"source /opt/ros/noetic/setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source /opt/ros/noetic/setup.bash\" >> ~/ros_environment.bash"
+sh -c "echo \"source \$HOME/ros_environment.bash\" >> ~/.bashrc"
 source /opt/ros/noetic/setup.bash
 source $HOME/.bashrc
 
@@ -39,8 +48,8 @@ echo "[Initialize rosdep and Update]"
 sudo sh -c "rosdep init"
 rosdep update
 
-sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
-sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
+sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/ros_environment.bash"
+sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/ros_environment.bash"
 
 echo "[Complete!!!]"
 exit 0
